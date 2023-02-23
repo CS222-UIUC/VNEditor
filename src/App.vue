f5d971ecf3be4109644a01c17ad015733cc14050
Tesseract22 <caleb2222@qq.com>
2023-02-23 04:11:10 +0000

    upload area

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
-<script setup lang="ts">
+
+
+<script>
+
 import Toolbar from "./components/Toolbar.vue";
 import Navbar from "./components/Navbar.vue"
+import FileUploadArea from "./components/UploadArea.vue"
+import Editor from "./components/Editor.vue"
+
+export default {
+  data() {
+    return {
+      fileUploadAreaDisplay: false
+    }
+  },
+  components: {
+    Toolbar,
+    Navbar,
+    FileUploadArea,
+    Editor,
+  },
+
+}
 </script>
 
 
@@ -8,11 +28,13 @@ import Navbar from "./components/Navbar.vue"
   <Navbar >
 
   </Navbar>
-  <main id="main-editor" class="grid-item">
-    This is main editor
+  <main id="main-editor" class="grid-item" @clcik="fileUploadAreaDisplay = true" @dragenter="fileUploadAreaDisplay = true" @dragexit="fileUploadAreaDisplay = false">
+    <FileUploadArea :fileUploadAreaDisplay="true" />
   </main>
   <div id="preview-sidebar" class="grid-item">this is sidebar 1</div>
-  <div id="toolbar-sidebar" class="grid-item">this is sidebar 2</div>
+  <div id="toolbar-sidebar" class="grid-item">
+    
+  </div>
   <footer id="footer" class="grid-item">this is footer</footer>
   
 
@@ -28,6 +50,7 @@ import Navbar from "./components/Navbar.vue"
 
 #main-editor {
   background: azure;
+  display: flex;
 }
 
 #preview-sidebar {
