1b1751a07927cb9fdf9cd0bddf3e3987d9f835c4
Tesseract22 <caleb2222@qq.com>
2023-03-02 23:13:34 -0600

    aa

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
-import item from "../components/NavbarItem.vue"
+import item from "../components/NavbarItem.vue";
 </script>
 
-
-
 <template>
-    <header id="header" class="grid-item">
-        <item></item>
-        <item></item>
-        <item></item>
-    </header>
-
+  <header id="header" class="grid-item">
+    <item></item>
+    <item></item>
+    <item></item>
+  </header>
 </template>
 
-
-
 <style>
-
 #header {
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
-
 </style>
