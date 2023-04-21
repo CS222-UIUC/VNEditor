f1ad72d43983bccb00e6b34be036c3d49c15b217
capTony02Zheng <363411246@qq.com>
2023-04-21 16:57:31 -0500

    shit

--- src/components/Chapter/ChapterDir.vue
+++ src/components/Chapter/ChapterDir.vue
@@ -5,31 +5,35 @@ import type { Ref } from "vue";
 import IconDownArrow from "../icons/IconDownArrow.vue";
 import type { IFrame } from "@/FrameDef";
 import { projectIDKey } from "../../InjectionKeys";
-import { getFrames } from "../../RequestAPI"; // need to change to needed function
-
+import { getFrames, addFrame } from "../../RequestAPI"; // need to change to needed function
 var FramesDisplay = ref(false); // control display the scene of the corresopnding chapter
+var FrameCreateDisplay = ref(false);
 const FrameList: Ref<IFrame[]> = ref([]);
 const chapName: Ref<string> = ref("");
-
-const props = defineProps({
-    ChapterName: {
-        type: ref<String>,
-        default: "wtf? should be the name of the chapter",
-    },
-    itemCallBack: {
-        type: Function as PropType<(event: MouseEvent) => void>,
-        default: () => console.log("CallBack in frames undefined"),
-    },
-});
+var frameNametoAdd = "";
+// function addFrame(idx: Number): boolean {
+//     return false;
+// }
+// function removeFrame(idx: Number): boolean {
+//     return false;
+// }
+// function switchBranch(id: FrameID): boolean {
+//     return false;
+// }
 const projectID = inject(projectIDKey) as Ref<string | undefined>;
-
 function updateChapName(event: MouseEvent) {
     const el = event.target as HTMLElement;
     const name = el.textContent;
     if (name) chapName.value = name;
     FramesDisplay.value = !FramesDisplay.value;
+    FrameCreateDisplay.value = false;
 }
-
+async function appendNewFrame(chap_name: string, frame_name: string) {
+    let result = await addFrame(projectID.value, chap_name, frame_name);
+    if (result != "") {
+        FrameCreateDisplay.value = !FrameCreateDisplay.value;
+    }
+}
 watchEffect(() => {
     // call back method update the chapter to display once projectID received
     if (projectID.value)
@@ -37,8 +41,9 @@ watchEffect(() => {
             if (res) FrameList.value = res;
             console.log("nextline is chapter name");
             console.log(chapName.value);
-            console.log(props.ChapterName); // sth wrong with here
+            // console.log(" ");
         });
+    frameNametoAdd;
 });
 </script>
 
@@ -53,13 +58,36 @@ watchEffect(() => {
         </div>
         <Transition name="drop">
             <div class="file-content-wrapper" v-show="FramesDisplay">
-                <FrameItem
-                    @click="itemCallBack($event)"
-                    v-for="item in FrameList"
-                    :name="item.name"
-                    :key="item.name"
-                    >{{ item.name }}</FrameItem
-                >
+                <div v-show="!FrameCreateDisplay">
+                    <button
+                        class="chapter-button"
+                        @click="FrameCreateDisplay = !FrameCreateDisplay"
+                    >
+                        New Frame
+                    </button>
+                </div>
+                <div class="addframe-wrapper" v-show="FrameCreateDisplay">
+                    <input
+                        class="add-frame-text"
+                        v-model="frameNametoAdd"
+                        placeholder="enter chapter name"
+                    />
+                    <button
+                        class="add-frame-button"
+                        @click="appendNewFrame(chapName, frameNametoAdd)"
+                    >
+                        Done
+                    </button>
+                    <button
+                        class="add-frame-button"
+                        @click="FrameCreateDisplay = !FrameCreateDisplay"
+                    >
+                        Cancle
+                    </button>
+                </div>
+                <FrameItem v-for="item in FrameList" :key="item.name" :name="item.name"
+                    >{{ item.name }}
+                </FrameItem>
             </div>
         </Transition>
     </div>
@@ -69,19 +97,16 @@ watchEffect(() => {
 .chapter-item {
     display: flex;
     flex-direction: column;
-    overflow: hidden;
     padding: 0.5rem;
     border-bottom: 5px solid rgba(0, 90, 27, 0.507);
     width: 100%;
 }
-
 /*icon part below*/
 .chap-icon-wrapper > svg {
     margin-top: auto;
     width: 2rem;
     height: 2rem;
 }
-
 .chap-icon-wrapper {
     overflow: hidden;
     text-align: left;
@@ -89,19 +114,41 @@ watchEffect(() => {
     flex-direction: row;
     font-size: x-large;
 }
-
 .chap-icon-wrapper:hover {
     text-align: left;
     background: rgb(45, 102, 34);
 }
-
 .up-rotated {
     transform: rotate(180deg);
     transition: transform 0.1s;
 }
-
 .down-rotated {
     transform: rotate(0deg);
     transition: transform 0.1s;
+}
+.addframe-wrapper {
+    flex-direction: row;
+    text-align: left;
+}
+.chapter-button {
+    display: flex;
+    flex-direction: column;
+    padding: 0.5rem;
+    border-bottom: 5px solid rgba(0, 90, 27, 0.507);
+    width: 100%;
+}
+.add-frame-text {
+    display: flex;
+    flex-direction: row;
+    padding: 0.5rem;
+    border-bottom: 5px solid rgba(0, 90, 27, 0.507);
+    width: 100%;
+}
+.add-frame-button {
+    display: flex;
+    flex-direction: row;
+    padding: 0.5rem;
+    border-bottom: 5px solid rgba(0, 90, 27, 0.507);
+    width: 100%;
 }
 </style>
