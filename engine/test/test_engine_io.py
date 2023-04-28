536e69495b95da06b4c4eee09f41ab9e6465af53
Tianyi Huang <tianyih5@illinois.edu>
2023-04-28 18:57:21 -0500

    add doc to testcase

--- engine/test/test_engine_io.py
+++ engine/test/test_engine_io.py
@@ -9,14 +9,28 @@ from kernel.engine_io import pickle_loader, pickle_dumper
 
 
 class TestPickleIO(unittest.TestCase):
+    """
+    A test case class for testing the pickle_loader and pickle_dumper functions.
+    """
+
     def setUp(self):
+        """
+        Set up the test data and create a temporary file for testing.
+        """
         self.test_data = [{"name": "item1", "value": 1}, {"name": "item2", "value": 2}]
         self.temp_file = tempfile.NamedTemporaryFile(delete=False)
 
     def tearDown(self):
+        """
+        Clean up by deleting the temporary file.
+        """
         os.unlink(self.temp_file.name)
 
     def test_pickle_loader_and_dumper(self):
+        """
+        Test case for pickle_loader and pickle_dumper functions.
+        It tests successful save and load operations, and also checks for proper handling of invalid file paths.
+        """
         # Test successful save and load
         pickle_dumper(self.test_data, self.temp_file.name)
         loaded_data = pickle_loader(self.temp_file.name)
@@ -27,8 +41,10 @@ class TestPickleIO(unittest.TestCase):
 
         # Test invalid file paths
         with self.assertRaises(FileNotFoundError):
+            # Attempt to load from an invalid file path
             pickle_loader("invalid/file/path")
         with self.assertRaises(FileNotFoundError):
+            # Attempt to save to an invalid file path
             pickle_dumper(self.test_data, "invalid/file/path")
 
 
--- engine/test/test_file_utils.py
+++ engine/test/test_file_utils.py
@@ -8,31 +8,72 @@ from utils import file_utils
 
 
 class TestFileUtils(TestCase):
+    """
+    A test case class for testing the functions in file_utils module.
+    """
+
     def test_check_file_valid(self):
+        """
+        Test case for check_file_valid function.
+        It checks the validity of a non-existing file and asserts False.
+        """
         self.assertFalse(file_utils.check_file_valid("aaaa"))
 
     def test_check_folder_valid(self):
+        """
+        Test case for check_folder_valid function.
+        It checks the validity of a non-existing folder and asserts False.
+        """
         self.assertFalse(file_utils.check_folder_valid("aaaa"))
 
     def test_get_files_in_folder(self):
+        """
+        Test case for get_files_in_folder function.
+        It checks the number of files in a non-existing folder and asserts 0.
+        It also checks the number of files in the current directory and asserts not equal to 0.
+        It checks the number of files in the current directory with a specific suffix and asserts 0.
+        """
         self.assertEqual(len(file_utils.get_files_in_folder("aaa")), 0)
         self.assertNotEqual(len(file_utils.get_files_in_folder("./")), 0)
         self.assertEqual(len(file_utils.get_files_in_folder("./", suffix=".py")), 0)
 
     def test_get_folders_in_folder(self):
+        """
+        Test case for get_folders_in_folder function.
+        It checks the number of folders in a non-existing folder and asserts 0.
+        It also checks the number of folders in the parent directory and asserts not equal to 0.
+        """
         self.assertEqual(len(file_utils.get_folders_in_folder("aaa")), 0)
         self.assertNotEqual(len(file_utils.get_folders_in_folder("../")), 0)
 
     def test_get_all_in_folder(self):
+        """
+        Test case for get_all_in_folder function.
+        It checks the number of files and folders in a non-existing folder and asserts 0.
+        """
         self.assertEqual(len(file_utils.get_all_in_folder("aaa")), 0)
 
     def test_get_folder_dir(self):
+        """
+        Test case for get_folder_dir function.
+        It checks the directory of a non-existing folder and asserts not None.
+        """
         self.assertIsNotNone(len(file_utils.get_folder_dir("aaa")))
 
     def test_get_ext(self):
+        """
+        Test case for get_ext function.
+        It checks the extension of a file and asserts not None.
+        """
         self.assertIsNotNone(len(file_utils.get_ext("aaa.txt")))
 
     def test_rename_file(self):
+        """
+        Test case for rename_file function.
+        It checks the renaming of a non-existing file and asserts False.
+        It creates a temporary file, renames it with a different extension,
+        and asserts True. Finally, it deletes the temporary file.
+        """
         self.assertFalse(file_utils.rename_file("aaaa", "bbbb"))
         fo = open("test_file", "w")
         fo.close()
@@ -41,9 +82,18 @@ class TestFileUtils(TestCase):
         file_utils.delete_file("test_file.txt")
 
     def test_delete_file(self):
+        """
+        Test case for delete_file function.
+        It checks the deletion of a non-existing file and asserts False.
+        """
         self.assertFalse(file_utils.delete_file("aaaaa"))
 
     def test_delete_folder(self):
+        """
+        Test case for delete_folder function.
+        It creates a temporary folder, deletes it, and asserts True.
+        It also checks the deletion of a non-existing folder and asserts False.
+        """
         os.mkdir("aaa")
         self.assertTrue(file_utils.delete_folder("aaa"))
         self.assertFalse(file_utils.delete_folder("aaaaaa"))
